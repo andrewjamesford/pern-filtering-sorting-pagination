bcaeadf17644f1e23a967641622bcf187cd6ac64
Andrew Ford <andrew@andrewford.co.nz>
2024-06-06 11:46:27 +1200

    Changes to regex patterns

--- server/src/products/product.router.js
+++ server/src/products/product.router.js
@@ -6,61 +6,61 @@ import productRepository from "./product.repository.js";
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-	try {
-		const products = await productRepository.getProducts();
+  try {
+    const products = await productRepository.getProducts();
 
-		const responseResults = {
-			products,
-		};
+    const responseResults = {
+      products,
+    };
 
-		return res.json(responseResults);
-	} catch (err) {
-		next(err);
-	}
+    return res.json(responseResults);
+  } catch (err) {
+    next(err);
+  }
 });
 
 const queryParamsSchema = Joi.object().keys({
-	sortOrder: Joi.string()
-		.pattern(/^[a-z0-9 ]+$/i)
-		.allow(null, ""),
-	direction: Joi.string()
-		.pattern(/^[a-z0-9 ]+$/i)
-		.allow(null, ""),
-	page: Joi.number().integer().min(0).required(),
-	pageSize: Joi.number().integer().min(1).required(),
-	priceRange: Joi.number().integer().min(20).max(100).required(),
+  sortOrder: Joi.string()
+    .pattern(/^[a-z]+$/i)
+    .allow(null, ""),
+  direction: Joi.string()
+    .pattern(/^[a-z]+$/i)
+    .allow(null, ""),
+  page: Joi.number().integer().min(0).required(),
+  pageSize: Joi.number().integer().min(1).required(),
+  priceRange: Joi.number().integer().min(20).max(100).required(),
 });
 
 router.get(
-	"/data/",
-	queryParamValidationMiddleware(queryParamsSchema),
-	async (req, res, next) => {
-		try {
-			const {
-				sortOrder = "name",
-				direction = "asc",
-				page = 0,
-				pageSize = 5,
-				priceRange = 20,
-			} = req.query;
+  "/data/",
+  queryParamValidationMiddleware(queryParamsSchema),
+  async (req, res, next) => {
+    try {
+      const {
+        sortOrder = "name",
+        direction = "asc",
+        page = 0,
+        pageSize = 5,
+        priceRange = 20,
+      } = req.query;
 
-			const products = await productRepository.getProductsPaginated(
-				sortOrder,
-				direction,
-				page,
-				pageSize,
-				priceRange,
-			);
+      const products = await productRepository.getProductsPaginated(
+        sortOrder,
+        direction,
+        page,
+        pageSize,
+        priceRange
+      );
 
-			const responseResults = {
-				products,
-			};
+      const responseResults = {
+        products,
+      };
 
-			return res.json(responseResults);
-		} catch (err) {
-			next(err);
-		}
-	},
+      return res.json(responseResults);
+    } catch (err) {
+      next(err);
+    }
+  }
 );
 
 export default router;
